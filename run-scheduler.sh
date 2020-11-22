a9b1c5abd3dfb53a2f69c5b8a34b63dc711350ac
Alexandre Bulté <alexandre@bulte.net>
2020-11-22 16:59:04 +0100

    only beat on scheduler

--- run-scheduler.sh
+++ run-scheduler.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # we need a wrapper for SCHEDULER=1
-SCHEDULER=1 python -m celery -b $DPP_CELERY_BROKER -A datapackage_pipelines.app -l INFO worker -B
+SCHEDULER=1 python -m celery -b $DPP_CELERY_BROKER -A datapackage_pipelines.app -l INFO beat
